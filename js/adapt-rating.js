d69252eeeb8b9152be54d16aad7d13957694f505
David Stump <david@davidstump.net>
2017-08-14 15:23:35 -0400

    Copy Gmcq file

--- js/adapt-rating.js
+++ js/adapt-rating.js
@@ -1,12 +1,85 @@
-  define([
-    'core/js/adapt',
-    'components/adapt-contrib-gmcq/js/adapt-contrib-gmcq',
+define([
+    'coreJS/adapt',
+    'components/adapt-contrib-mcq/js/adapt-contrib-mcq',
     './ratingView'
-], function(Adapt, Gmcq, RatingView) {
-      
+], function(Adapt, Mcq, RatingView) {
+
+    var Rating = Mcq.view.extend({
+
+        events: {
+            'focus .gmcq-item input': 'onItemFocus',
+            'blur .gmcq-item input': 'onItemBlur',
+            'change .gmcq-item input': 'onItemSelected',
+            'keyup .gmcq-item input': 'onKeyPress'
+        },
+
+        onItemSelected: function(event) {
+
+            var selectedItemObject = this.model.get('_items')[$(event.currentTarget).parent('.gmcq-item').index()];
+
+            if (this.model.get('_isEnabled') && !this.model.get('_isSubmitted')) {
+                this.toggleItemSelected(selectedItemObject, event);
+            }
+
+        },
+
+        setupQuestion: function() {
+            Mcq.view.prototype.setupQuestion.call(this);
+
+            this.listenTo(Adapt, {
+                'device:changed': this.resizeImage,
+                'device:resize': this.onDeviceResize
+            });
+
+        },
+
+        onQuestionRendered: function() {
+
+            this.resizeImage(Adapt.device.screenSize);
+            this.setUpColumns();
+
+            this.$('label').imageready(_.bind(function() {
+                this.setReadyStatus();
+            }, this));
+
+        },
+        
+        onDeviceResize: function() {
+            this.setUpColumns();
+        },
+
+        resizeImage: function(width) {
+
+            var imageWidth = width === 'medium' ? 'small' : width;
+
+            this.$('label').each(function(index) {
+                var src = $(this).find('img').attr('data-' + imageWidth);
+                $(this).find('img').attr('src', src);
+            });
+
+        },
+
+        setUpColumns: function() {
+            var columns = this.model.get('_columns');
+
+            if (!columns) return;
+
+            if (Adapt.device.screenSize === 'large') {
+                this.$el.addClass('gmcq-column-layout');
+                this.$('.gmcq-item').css('width', (100 / columns) + '%');
+            } else {
+                this.$el.removeClass('gmcq-column-layout');
+                this.$('.gmcq-item').css('width', '');
+            }
+        }
+
+    }, {
+        template: 'rating'
+    });
+
     return Adapt.register("rating", {
         view: RatingView,
-        model: Gmcq.model.extend({})
+        model: Mcq.model.extend({})
     });
 
 });
