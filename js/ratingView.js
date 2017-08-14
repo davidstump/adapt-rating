ea238ec460437c402efd90d1bb7fe99a238ebe8e
David Stump <david@davidstump.net>
2017-08-14 14:57:38 -0400

    Update Mcq view extension

--- js/ratingView.js
+++ js/ratingView.js
@@ -2,7 +2,7 @@ define([
     'components/adapt-contrib-mcq/js/mcqView'
 ], function(Adapt, McqView) {
 
-    var RatingView = QuestionView.extend({
+    var RatingView = McqView.extend({
 
         /**
         * used by adapt-contrib-spoor to get the type of this question in the format required by the cmi.interactions.n.type data field
